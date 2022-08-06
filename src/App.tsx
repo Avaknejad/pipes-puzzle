6ea0bdcc93a8d736773dc7467ea33b6c4d05cfc4
Ava kazeminejad <101579399+Avaknejad@users.noreply.github.com>
2022-08-06 14:48:09 +0430

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>pipes puzzle game</title>
+      <link rel="stylesheet" href="puzzle.css" />
+      <script src="puzzle.js"></script>
+      <meta name="description" />
+    </head>
+    <body>
+      <img id="title" src="logo.png" />
+      <div id="board">
+        <div class="container">
+          <div class="img-container">
+            <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+            <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+            <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+              <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+              <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+              <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+              <img width="40" height="40" src="1.png" /></div>
+          <div class="img-container">
+              <img width="40" height="40" src="1.png" /></div>
+            <div class="img-container">
+                <img width="40" height="40" src="1.png" /></div>
+            <div class="img-container">
+                  <img width="40" height="40" src="1.png" /></div>
+        </div>
+      </div>
+      <h1>level:<span id="level">1</span></h1>
+    </body>
+  </html>
+</html>
