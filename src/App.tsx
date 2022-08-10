03f1344bd623f96705f1f494c5c3673096a9a0de
Pourya Daneshvar <pourya@daneshvar.me>
2022-08-10 12:40:44 +0430

    Prepare for start

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,63 @@
-<!DOCTYPE html>
-<html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>pipes puzzle game</title>
-      <link rel="stylesheet" href="puzzle.css" />
-      <script src="puzzle.js"></script>
-      <meta name="description" />
-    </head>
-    <body>
+import "./App.css";
+
+const map = `
+map:
+┓╻╹┻╸┛┃┏
+┗┏┗┳┛┛┓╻
+╹╋┗╻┫┃┏╻
+┛┻━┻╹╻┛┛
+┛━┏━╸┳┗┏
+╻╹┣╸┏┻┃┛
+┻━┻━╋━━┓
+┗┃╹╺┫━╺╸`;
+
+const imageMapAssign = {
+  0 : '┻',
+  1 : '┛'
+}
+
+const App = () => {
+  const level = 1
+
+  const renderMap = () => {
+    // TODO:
+    // Parse string to array
+
+    // return [<div class="img-container"><img width="40" height="40" src="1.png" /></div>]
+  }
+  const rotate = (x:number, y:number) => {
+    // TODO:
+    // 1. Rotate tile by JS & CSS
+  };
+  
+
+  // TODO: convert map to string array
+  // const map:string[] = map ...
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+  const Tile = () => (
+    <div className="img-container">
+      <img width="40" height="40" src="1.png" />
+    </div>
+  );
+
+  return (
+    <div>
       <img id="title" src="logo.png" />
       <div id="board">
-        <div class="container">
-          <div class="img-container">
-            <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-            <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-            <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-              <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-              <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-              <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-              <img width="40" height="40" src="1.png" /></div>
-          <div class="img-container">
-              <img width="40" height="40" src="1.png" /></div>
-            <div class="img-container">
-                <img width="40" height="40" src="1.png" /></div>
-            <div class="img-container">
-                  <img width="40" height="40" src="1.png" /></div>
+        <div className="container">
+          {
+            /* TODO: 
+              Render Tiles by user level number
+            */
+          }
+         <Tile />
         </div>
       </div>
-      <h1>level:<span id="level">1</span></h1>
-    </body>
-  </html>
-</html>
+      <h1>
+        level:<span id="level">1</span>
+      </h1>
+    </div>
+  );
+};
+export default App;
